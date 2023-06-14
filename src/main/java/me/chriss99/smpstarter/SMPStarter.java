e2608c80d2173cf2d6f7ace2efa802605f8a0fc0
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2023-06-14 15:52:09 +0200

    setup commit

---
+++ src/main/java/me/chriss99/smpstarter/SMPStarter.java
@@ -0,0 +1,17 @@
+package me.chriss99.smpstarter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPStarter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
